1073f77e95fea82dd75cc71963b8a611622edbcc
Mohamed Atef <proofesser@gmail.com>
2023-12-10 10:41:04 +0200

    Added MyApp as a Name for Application tag in xml - for Hilt

--- app/src/main/java/mo/ed/bankmisr/MyApp.kt
+++ app/src/main/java/mo/ed/bankmisr/MyApp.kt
@@ -2,13 +2,16 @@ package mo.ed.bankmisr
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
 
 
 @HiltAndroidApp
 class MyApp : Application() {
+
     override fun onCreate() {
         super.onCreate()
-
-
     }
+
+
 }
